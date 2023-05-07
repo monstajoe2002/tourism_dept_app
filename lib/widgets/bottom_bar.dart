5a71ab4f4f1e1b6f38bdb347d6d73c8509ec25d4
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-05-07 17:06:45 +0300

    feature: finished bottom bar

--- lib/widgets/bottom_bar.dart
+++ lib/widgets/bottom_bar.dart
@@ -18,13 +18,19 @@ class _BottomBarState extends State<BottomBar> {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      items: [
+      items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
         BottomNavigationBarItem(
             icon: Icon(Icons.favorite_rounded), label: 'Favorites'),
         BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline_rounded), label: 'New Post'),
+        BottomNavigationBarItem(
             icon: Icon(Icons.notifications), label: 'Notifications'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded), label: 'Profile'),
       ],
+      type: BottomNavigationBarType
+          .fixed, // this is added when the items in the bottom bar are more than 3
       currentIndex: _selectedIndex,
       onTap: _onItemTapped,
     );
