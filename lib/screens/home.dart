ec489a59869204918d794e5a406c12ba47503424
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-05-07 22:06:19 +0300

    refactor: added a scrollview to the home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,30 +14,32 @@ class Home extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Home'),
         ),
-        body: Container(
-            padding: const EdgeInsets.all(30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Hi there 👋', style: TextStyle(fontSize: 20.0)),
-                const Text(
-                  'Take a virtual museum tour',
-                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
-                ),
-                const SearchBox(),
-                const Categories(),
-                Container(
-                  margin: const EdgeInsets.only(top: 30),
-                  child: Center(
-                      child: GestureDetector(
-                    child: const PostCard(),
-                    onTap: () {
-                      debugPrint('PostCard tapped');
-                    },
-                  )),
-                )
-              ],
-            )),
+        body: SingleChildScrollView(
+          child: Container(
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Hi there 👋', style: TextStyle(fontSize: 20.0)),
+                  const Text(
+                    'Take a virtual museum tour',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
+                  ),
+                  const SearchBox(),
+                  const Categories(),
+                  Container(
+                    margin: const EdgeInsets.only(top: 30),
+                    child: Center(
+                        child: GestureDetector(
+                      child: const PostCard(),
+                      onTap: () {
+                        debugPrint('PostCard tapped');
+                      },
+                    )),
+                  )
+                ],
+              )),
+        ),
         bottomNavigationBar: const BottomBar());
   }
 }
